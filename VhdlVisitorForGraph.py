472d488aed5400f817ff45cc1094cd967f9e6ecf
popas <bogdititupopa@gmail.com>
2015-09-08 23:08:53 +0300

    visitor change

--- VhdlVisitorForGraph.py
+++ VhdlVisitorForGraph.py
@@ -1,9 +1,17 @@
 from VhdlVisitor import VhdlVisitor
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .VhdlParser import VhdlParser
+else:
+    from VhdlParser import VhdlParser
 from VhdlParser import VhdlParser
 
-class VhdlVisitorForGraph(VhdlVisitor):
+class VhdlVisitorForGraph(ParseTreeVisitor):
 
     # Visit a parse tree produced by VhdlParser#identifier.
     def visitIdentifier(self, ctx:VhdlParser.IdentifierContext):
-        print(ctx.BASIC_IDENTIFIER())
-        print(ctx.EXTENDED_IDENTIFIER())
+        if (ctx.BASIC_IDENTIFIER()):
+            print(ctx.BASIC_IDENTIFIER())
+
+        if (ctx.EXTENDED_IDENTIFIER()):
+            print(ctx.EXTENDED_IDENTIFIER())
