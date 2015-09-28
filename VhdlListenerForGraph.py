57fcba427961be5d852e20b3635bca606327e1c4
popas <bogdititupopa@gmail.com>
2015-09-28 22:51:36 +0300

    now using namedtuples

--- VhdlListenerForGraph.py
+++ VhdlListenerForGraph.py
@@ -3,9 +3,7 @@ from VhdlListener import VhdlListener
 from VhdlParser import VhdlParser
 from Component import Component
 from Deque import Deque
-from Label import Label
-from StringContainer import StringContainer
-from StringContainer import StringContainerCategory
+from collections import namedtuple
 
 
 # This class defines a complete listener for a parse tree produced by VhdlParser.
@@ -13,6 +11,7 @@ class VhdlListenerForGraph(VhdlListener):
 
     def __init__(self):
         self.parsing_stack = Deque()
+        # TODO maybe a hashmap would be better here ?
         self.components_list = []
 
     # Component instantiation statement
@@ -26,61 +25,67 @@ class VhdlListenerForGraph(VhdlListener):
 
     # Label colon
     def enterLabel_colon(self, ctx:VhdlParser.Label_colonContext):
-        self.parsing_stack.push(StringContainer(StringContainerCategory.Label))
+        self.parsing_stack.push(namedtuple("Label", "identifier"))
 
     def exitLabel_colon(self, ctx:VhdlParser.Label_colonContext):
         label = self.parsing_stack.pop()
-        self.parsing_stack.preview_top().set_label(label.value)
+        self.parsing_stack.preview_top().set_label(label.identifier)
 
     # Instantiation unit
     def enterInstantiated_unit(self, ctx:VhdlParser.Instantiated_unitContext):
-        self.parsing_stack.push(StringContainer(StringContainerCategory.Instantiated_Unit))
+        self.parsing_stack.push(namedtuple("InstantiatedUnit", "identifier"))
 
     def exitInstantiated_unit(self, ctx:VhdlParser.Instantiated_unitContext):
         instantiated_unit = self.parsing_stack.pop()
-        self.parsing_stack.preview_top().set_name(instantiated_unit.value)
+        self.parsing_stack.preview_top().set_name(instantiated_unit.identifier)
 
     # Name
     def enterName(self, ctx:VhdlParser.NameContext):
-        self.parsing_stack.push(StringContainer(StringContainerCategory.Name))
+        self.parsing_stack.push(namedtuple("Name", "identifier"))
 
     def exitName(self, ctx:VhdlParser.NameContext):
         name = self.parsing_stack.pop()
         # TODO see what kind of node this is
         # For now, assume it's an instantiated unit
-        self.parsing_stack.preview_top().set_identifier(name.value)
+        self.parsing_stack.preview_top().set_identifier(name.identifier)
 
     # Identifier
     def enterIdentifier(self, ctx:VhdlParser.IdentifierContext):
-        self.parsing_stack.preview_top().set_identifier(ctx.BASIC_IDENTIFIER())
+        self.parsing_stack.preview_top().identifier = ctx.BASIC_IDENTIFIER()
 
     def exitIdentifier(self, ctx:VhdlParser.IdentifierContext):
         pass
 
     # Port map aspect
     def enterPort_map_aspect(self, ctx:VhdlParser.Port_map_aspectContext):
-        pass
+        self.parsing_stack.push(namedtuple("PortMap", "associations"))
+        self.parsing_stack.preview_top().associations = []
 
     def exitPort_map_aspect(self, ctx:VhdlParser.Port_map_aspectContext):
-        pass
+        port_map = self.parsing_stack.pop()
+        # TODO add port_map property to Component as a list of associations
+        self.parsing_stack.preview_top().port_map = port_map
 
     # Association element
     def enterAssociation_element(self, ctx:VhdlParser.Association_elementContext):
-        pass
+        self.parsing_stack.push(namedtuple("Association", ["formal", "actual"]))
 
     def exitAssociation_element(self, ctx:VhdlParser.Association_elementContext):
-        pass
+        association = self.parsing_stack.pop()
+        self.parsing_stack.preview_top().associations.push(association)
 
     # Formal part
     def enterFormal_part(self, ctx:VhdlParser.Formal_partContext):
-        pass
+        self.parsing_stack.push(namedtuple("Formal", "identifier"))
 
     def exitFormal_part(self, ctx:VhdlParser.Formal_partContext):
-        pass
+        formal_part = self.parsing_stack.pop()
+        self.parsing_stack.preview_top().formal = formal_part.identifier
 
     # Actual part
     def enterActual_part(self, ctx:VhdlParser.Actual_partContext):
-        pass
+        self.parsing_stack.push(namedtuple("Actual", "identifier"))
 
     def exitActual_part(self, ctx:VhdlParser.Actual_partContext):
-        pass
+        actual_part = self.parsing_stack.pop()
+        self.parsing_stack.preview_top().actual = actual_part.identifier
