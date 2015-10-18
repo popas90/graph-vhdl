78b23cbf2f9a8aaa010b28496d82d666701e9a09
popas90 <bogdititupopa@gmail.com>
2015-10-18 09:48:28 +0300

    added another test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -16,3 +16,16 @@ def test_identify_instances():
     walker = ParseTreeWalker()
     walker.walk(listener, tree)
     nose.tools.eq_(len(listener.components_list), 2)
+
+
+def test_no_instances():
+    inp = FileStream('./tests/testfiles/d_flop_behav.vhd')
+    lexer = VhdlLexer(inp)
+    stream = CommonTokenStream(lexer)
+    parser = VhdlParser(stream)
+    listener = VhdlListenerForGraph()
+    parser._interp.predictionMode = PredictionMode.LL
+    tree = parser.design_file()
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    nose.tools.eq_(len(listener.components_list), 0)
