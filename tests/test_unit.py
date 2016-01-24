4bb853b6c020e8bb5d853d46d0c16ca27cbb552e
popas90 <bogdititupopa@gmail.com>
2016-01-24 22:45:11 +0200

    added new test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -18,12 +18,12 @@ def parse_file_setup(file_path):
     return listener
 
 
-def test_identify_instances():
+def test_two_instances():
     listener = parse_file_setup('./tests/testfiles/struct_with_components.vhd')
     nose.tools.eq_(len(listener.instances_list), 2)
 
 
-def test_no_instances():
+def test_zero_instances():
     listener = parse_file_setup('./tests/testfiles/d_flop_behav.vhd')
     nose.tools.eq_(len(listener.instances_list), 0)
 
@@ -31,3 +31,8 @@ def test_no_instances():
 def test_one_process():
     listener = parse_file_setup('./tests/testfiles/ac_behave.vhd')
     nose.tools.eq_(len(listener.processes_list), 1)
+
+
+def test_zero_processes():
+    listener = parse_file_setup('./tests/testfiles/struct_with_components.vhd')
+    nose.tools.eq_(len(listener.processes_list), 0)
